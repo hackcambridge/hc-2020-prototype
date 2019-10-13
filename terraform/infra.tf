56c8158b8c84669962cec6502be89c527eda5310
Harri Bell-Thomas <harribt@live.co.uk>
2019-10-13 23:08:11 +0100

    Tweak to infra.tf

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -4,18 +4,17 @@ provider "aws" {
 
 data "template_file" "user_data" {
   template = <<EOF
-  #!/bin/bash
-sudo su
-/opt/bitnami/ctlscript.sh stop
-apt-get -y update
-apt-get -y install ruby
-apt-get -y install wget
-cd /home/bitnami
-wget https://aws-codedeploy-eu-west-2.s3.amazonaws.com/latest/install
-chmod +x ./install
-./install auto
-exit
-EOF
+    #!/bin/bash
+    sudo su
+    apt-get -y update
+    apt-get -y install ruby
+    apt-get -y install wget
+    cd /home/bitnami
+    wget https://aws-codedeploy-eu-west-2.s3.amazonaws.com/latest/install
+    chmod +x ./install
+    ./install auto
+    exit
+  EOF
 }
 
 resource "aws_launch_template" "hc-instance" {
