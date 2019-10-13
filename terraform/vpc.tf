7ab1aa57bec065bfd970a83fa7ce7fa90bae63e6
Timothy Lazarus <me@timlazarus.com>
2019-10-13 13:56:16 +0100

    Fix all protocol error (#18)

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "hc-sg-web" {
 
   egress {
     from_port       = 0
-    to_port         = 65535
+    to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
