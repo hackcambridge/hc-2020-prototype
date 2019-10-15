1799eafcbd6b492e5a14d35b4fbbae81e27849ba
Timothy Lazarus <me@timlazarus.com>
2019-10-15 13:59:15 +0100

    Fix stuff (#28)
    * Move setup script to seperate file
    
    * Remove all setup temporarily to debug setup process
    
    * Re-enable startup script
    
    * Remove grace period in start up
    
    * Fix startup script

--- terraform/init-script.sh
+++ terraform/init-script.sh
@@ -5,6 +5,7 @@ apt-get -y update
 apt-get -y install ruby
 apt-get -y install wget
 cd /home/bitnami
+wget https://aws-codedeploy-eu-west-2.s3.amazonaws.com/latest/install
 chmod +x ./install
 ./install auto
 exit
