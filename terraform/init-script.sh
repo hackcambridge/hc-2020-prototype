928ad729b4b0a61db63202fce3f9761667f8a9f5
Tim Lazarus <me@timlazarus.com>
2019-10-15 13:35:11 +0100

    Re-enable startup script

--- terraform/init-script.sh
+++ terraform/init-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# sudo su
-# /opt/bitnami/ctlscript.sh stop
-# apt-get -y update
-# apt-get -y install ruby
-# apt-get -y install wget
-# cd /home/bitnami
-# wget https://aws-codedeploy-eu-west-2.s3.amazonaws.com/latest/install
-# chmod +x ./install
-# ./install auto
-# exit
+sudo su
+/opt/bitnami/ctlscript.sh stop
+apt-get -y update
+apt-get -y install ruby
+apt-get -y install wget
+cd /home/bitnami
+
+chmod +x ./install
+./install auto
+exit
