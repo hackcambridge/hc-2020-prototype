7a1127550be9ec08823ba1dcdfe8a58b63a264a6
Tim Lazarus <me@timlazarus.com>
2019-10-16 09:59:36 +0100

    Format Terraform according to standards

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -5,25 +5,25 @@ provider "aws" {
 data "template_file" "user_data" {
   template = "${file("init-script.tpl")}"
   vars = {
-    APP_URL = "${var.APP_URL}"
-    DB_HOST = "${var.DB_HOST}"
-    DB_DATABASE = "${var.DB_DATABASE}"
-    DB_USERNAME = "${var.DB_USERNAME}"
-    DB_PASSWORD = "${var.DB_PASSWORD}"
-    AWS_ACCESS_KEY_ID = "${var.AWS_ACCESS_KEY_ID}"
+    APP_URL               = "${var.APP_URL}"
+    DB_HOST               = "${var.DB_HOST}"
+    DB_DATABASE           = "${var.DB_DATABASE}"
+    DB_USERNAME           = "${var.DB_USERNAME}"
+    DB_PASSWORD           = "${var.DB_PASSWORD}"
+    AWS_ACCESS_KEY_ID     = "${var.AWS_ACCESS_KEY_ID}"
     AWS_SECRET_ACCESS_KEY = "${var.AWS_SECRET_ACCESS_KEY}"
-    AWS_BUCKET = "${var.AWS_BUCKET}"
-    AUTH0_DOMAIN = "${var.AUTH0_DOMAIN}"
-    AUTH0_CLIENT_ID = "${var.AUTH0_CLIENT_ID}"
-    AUTH0_CLIENT_SECRET = "${var.AUTH0_CLIENT_SECRET}"
+    AWS_BUCKET            = "${var.AWS_BUCKET}"
+    AUTH0_DOMAIN          = "${var.AUTH0_DOMAIN}"
+    AUTH0_CLIENT_ID       = "${var.AUTH0_CLIENT_ID}"
+    AUTH0_CLIENT_SECRET   = "${var.AUTH0_CLIENT_SECRET}"
   }
 }
 
 resource "aws_launch_template" "hc-instance" {
-  name_prefix = "hc-instance"
-  image_id = "ami-040eaa068dbe2517d"
-  instance_type = "t2.micro"
-  key_name = "Default"
+  name_prefix            = "hc-instance"
+  image_id               = "ami-040eaa068dbe2517d"
+  instance_type          = "t2.micro"
+  key_name               = "Default"
   vpc_security_group_ids = ["${aws_security_group.hc-sg-web.id}"]
   lifecycle {
     create_before_destroy = true
@@ -36,29 +36,29 @@ resource "aws_launch_template" "hc-instance" {
 
 resource "aws_autoscaling_group" "front-end" {
   availability_zones = ["eu-west-2a", "eu-west-2b"]
-  min_size = 1
-  max_size = 2
-  desired_capacity = 2
+  min_size           = 1
+  max_size           = 2
+  desired_capacity   = 2
   # asg_recreate_on_change = false
   launch_template {
-    id = "${aws_launch_template.hc-instance.id}"
+    id      = "${aws_launch_template.hc-instance.id}"
     version = "$Latest"
   }
 }
 
 resource "aws_lb" "front-end-lb" {
-  name = "front-end-lb"
-  internal = false
-  load_balancer_type = "network"
-  subnets = ["${aws_default_subnet.default_A.id}", "${aws_default_subnet.default_B.id}"]
+  name                             = "front-end-lb"
+  internal                         = false
+  load_balancer_type               = "network"
+  subnets                          = ["${aws_default_subnet.default_A.id}", "${aws_default_subnet.default_B.id}"]
   enable_cross_zone_load_balancing = true
 }
 
 resource "aws_lb_target_group" "front-end-lb-target-group" {
-  name = "front-end-lb-target-group"
-  port = "80"
-  protocol = "TCP"
-  vpc_id = "${aws_default_vpc.default.id}"
+  name                 = "front-end-lb-target-group"
+  port                 = "80"
+  protocol             = "TCP"
+  vpc_id               = "${aws_default_vpc.default.id}"
   deregistration_delay = 30
 
   # health_check {
@@ -73,17 +73,17 @@ resource "aws_lb_target_group" "front-end-lb-target-group" {
 
 resource "aws_alb_listener" "front_end-lb-listener" {
   load_balancer_arn = "${aws_lb.front-end-lb.arn}"
-  port = 80
-  protocol = "TCP"
+  port              = 80
+  protocol          = "TCP"
 
   default_action {
     target_group_arn = "${aws_lb_target_group.front-end-lb-target-group.arn}"
-    type= "forward"
+    type             = "forward"
   }
 }
 
 resource "aws_autoscaling_attachment" "front-end-lb-autoscaling" {
-  alb_target_group_arn = "${aws_lb_target_group.front-end-lb-target-group.arn}"
+  alb_target_group_arn   = "${aws_lb_target_group.front-end-lb-target-group.arn}"
   autoscaling_group_name = "${aws_autoscaling_group.front-end.id}"
 }
 
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -21,48 +21,48 @@ resource "aws_default_subnet" "default_B" {
 }
 
 resource "aws_security_group" "hc-sg-web" {
-  name = "hc-sg-web"
+  name        = "hc-sg-web"
   description = "Hack Cambridge Web Servers"
-  vpc_id = "${aws_default_vpc.default.id}"
+  vpc_id      = "${aws_default_vpc.default.id}"
 
   ingress {
     # TLS (change to whatever ports you need)
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
     # TLS (change to whatever ports you need)
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
     # TLS (change to whatever ports you need)
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 }
