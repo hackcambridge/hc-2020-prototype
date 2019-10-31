a86be65058bd5c1f9d4f04b9a7b3a2ff0329a994
Carlos <virtcarlos@c0670.int.cl.cam.ac.uk>
2019-10-31 16:19:12 +0000

    Fix AddAllowedEmail

--- app/Http/Controllers/Sponsors.php
+++ app/Http/Controllers/Sponsors.php
@@ -168,18 +168,19 @@ class Sponsors extends Controller
         }
     }
 
-    public function addAllowedEmail($email, $name, $username, $password){
+    public function addAllowedEmail($email, $name){
       // TODO: Add Bearer/auth
       // ---> Maybe don't use cURL
       $ch = curl_init();
 
-      curl_setopt($ch, CURLOPT_URL, 'https://login.auth0.com/api/v2/users');
+      curl_setopt($ch, CURLOPT_URL, 'https://hackcambridge.eu.auth0.com/api/v2/users');
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"email\":\"".$email."\",\"user_metadata\":{},\"blocked\":false,\"name\":\"".$name."\",\"user_id\":\"abc\",\"connection\":\"Initial-Connection\",\"password\":\"".$password."\",\"verify_email\":true,\"username\":\"".$username."\"}");
+      curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"email\":\".$email.\",\"user_metadata\":{},\"blocked\":false,\"email_verified\":true,\"app_metadata\":{},\"name\":\"".$name."\",\"connection\":\"email\",\"verify_email\":false}");
 
       $headers = array();
       $headers[] = 'Content-Type: application/json';
+      $headers[] = 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5rSTVNVGcxTVRRMk5rVXdNVGd5UlRNM1FqUkZRVEF3TWpBNE9FVXhSakkyT1VaQ01EVXhNdyJ9.eyJpc3MiOiJodHRwczovL2hhY2tjYW1icmlkZ2UuZXUuYXV0aDAuY29tLyIsInN1YiI6IncyS1hta3QwV2NmRGZzbjJ4WlVBN0xTVm50cHY5TmRTQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2hhY2tjYW1icmlkZ2UuZXUuYXV0aDAuY29tL2FwaS92Mi8iLCJpYXQiOjE1NzI1MzczNDksImV4cCI6MTU3MjYyMzc0OSwiYXpwIjoidzJLWG1rdDBXY2ZEZnNuMnhaVUE3TFNWbnRwdjlOZFMiLCJzY29wZSI6InJlYWQ6Y2xpZW50X2dyYW50cyBjcmVhdGU6Y2xpZW50X2dyYW50cyBkZWxldGU6Y2xpZW50X2dyYW50cyB1cGRhdGU6Y2xpZW50X2dyYW50cyByZWFkOnVzZXJzIHVwZGF0ZTp1c2VycyBkZWxldGU6dXNlcnMgY3JlYXRlOnVzZXJzIHJlYWQ6dXNlcnNfYXBwX21ldGFkYXRhIHVwZGF0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgZGVsZXRlOnVzZXJzX2FwcF9tZXRhZGF0YSBjcmVhdGU6dXNlcnNfYXBwX21ldGFkYXRhIGNyZWF0ZTp1c2VyX3RpY2tldHMgcmVhZDpjbGllbnRzIHVwZGF0ZTpjbGllbnRzIGRlbGV0ZTpjbGllbnRzIGNyZWF0ZTpjbGllbnRzIHJlYWQ6Y2xpZW50X2tleXMgdXBkYXRlOmNsaWVudF9rZXlzIGRlbGV0ZTpjbGllbnRfa2V5cyBjcmVhdGU6Y2xpZW50X2tleXMgcmVhZDpjb25uZWN0aW9ucyB1cGRhdGU6Y29ubmVjdGlvbnMgZGVsZXRlOmNvbm5lY3Rpb25zIGNyZWF0ZTpjb25uZWN0aW9ucyByZWFkOnJlc291cmNlX3NlcnZlcnMgdXBkYXRlOnJlc291cmNlX3NlcnZlcnMgZGVsZXRlOnJlc291cmNlX3NlcnZlcnMgY3JlYXRlOnJlc291cmNlX3NlcnZlcnMgcmVhZDpkZXZpY2VfY3JlZGVudGlhbHMgdXBkYXRlOmRldmljZV9jcmVkZW50aWFscyBkZWxldGU6ZGV2aWNlX2NyZWRlbnRpYWxzIGNyZWF0ZTpkZXZpY2VfY3JlZGVudGlhbHMgcmVhZDpydWxlcyB1cGRhdGU6cnVsZXMgZGVsZXRlOnJ1bGVzIGNyZWF0ZTpydWxlcyByZWFkOnJ1bGVzX2NvbmZpZ3MgdXBkYXRlOnJ1bGVzX2NvbmZpZ3MgZGVsZXRlOnJ1bGVzX2NvbmZpZ3MgcmVhZDplbWFpbF9wcm92aWRlciB1cGRhdGU6ZW1haWxfcHJvdmlkZXIgZGVsZXRlOmVtYWlsX3Byb3ZpZGVyIGNyZWF0ZTplbWFpbF9wcm92aWRlciBibGFja2xpc3Q6dG9rZW5zIHJlYWQ6c3RhdHMgcmVhZDp0ZW5hbnRfc2V0dGluZ3MgdXBkYXRlOnRlbmFudF9zZXR0aW5ncyByZWFkOmxvZ3MgcmVhZDpzaGllbGRzIGNyZWF0ZTpzaGllbGRzIGRlbGV0ZTpzaGllbGRzIHJlYWQ6YW5vbWFseV9ibG9ja3MgZGVsZXRlOmFub21hbHlfYmxvY2tzIHVwZGF0ZTp0cmlnZ2VycyByZWFkOnRyaWdnZXJzIHJlYWQ6Z3JhbnRzIGRlbGV0ZTpncmFudHMgcmVhZDpndWFyZGlhbl9mYWN0b3JzIHVwZGF0ZTpndWFyZGlhbl9mYWN0b3JzIHJlYWQ6Z3VhcmRpYW5fZW5yb2xsbWVudHMgZGVsZXRlOmd1YXJkaWFuX2Vucm9sbG1lbnRzIGNyZWF0ZTpndWFyZGlhbl9lbnJvbGxtZW50X3RpY2tldHMgcmVhZDp1c2VyX2lkcF90b2tlbnMgY3JlYXRlOnBhc3N3b3Jkc19jaGVja2luZ19qb2IgZGVsZXRlOnBhc3N3b3Jkc19jaGVja2luZ19qb2IgcmVhZDpjdXN0b21fZG9tYWlucyBkZWxldGU6Y3VzdG9tX2RvbWFpbnMgY3JlYXRlOmN1c3RvbV9kb21haW5zIHJlYWQ6ZW1haWxfdGVtcGxhdGVzIGNyZWF0ZTplbWFpbF90ZW1wbGF0ZXMgdXBkYXRlOmVtYWlsX3RlbXBsYXRlcyByZWFkOm1mYV9wb2xpY2llcyB1cGRhdGU6bWZhX3BvbGljaWVzIHJlYWQ6cm9sZXMgY3JlYXRlOnJvbGVzIGRlbGV0ZTpyb2xlcyB1cGRhdGU6cm9sZXMgcmVhZDpwcm9tcHRzIHVwZGF0ZTpwcm9tcHRzIHJlYWQ6YnJhbmRpbmcgdXBkYXRlOmJyYW5kaW5nIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.PaLGF0UqWerhlEPokgYpZpSbXOFBlZUIgVTtL7trbszVo-LMIhp0HKAMij5rd7JVkVURhQbwOLYc-Om1VI_kbLUkmZ6JFSZN-MY1i4AuFvQzeJi-iwWbc5rruSEA1fy8-2rPHOCjwFF5Cq7EFfwMHEBQIDyvutCsAx6wv3VUBL6HYCHTts_-tqgtEfzQhBEZV3IUD5bzaurc2nvzqMZPCmyEba3BL-puDW3VlcPab-dbONpCXWT0gFnuJqqgrPJ2f7ZyTt_OOENS8V7VTbiDCSu8i1poYt1JKfvKIOBHeXwcQY-BbZ2Kc6DdUGiLWiieqZTaSmeP_VZ2KK1Mv9sOnA';
       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
       $result = curl_exec($ch);
