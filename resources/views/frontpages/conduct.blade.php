3d02960a4ff9496ecaa388225de62bcf0f8fd1b8
Tim Lazarus <me@timlazarus.com>
2019-11-26 13:14:54 +0000

    Add code of conduct

--- resources/views/frontpages/conduct.blade.php
+++ resources/views/frontpages/conduct.blade.php
@@ -9,9 +9,131 @@
 
 @section('content')
     <section id="" class="section-diagonal static-section">
-        <div class="container grid-lg" style="padding:2rem 30px 1rem;height:400px;">
+        <div class="container grid-lg" style="padding:2rem 30px 1rem;">
             <h1 class="section-header">Code of Conduct</h1>
+
+            <p style="text-align:center">
+              <i>This code of conduct has been taken from <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf">MLH</a></i>
+            </p>
+
+            <p>
+              <i><b>TL;DR.</b>Be respectful. Harassment and abuse are never tolerated.
+              If you are in a situation that makes you uncomfortable at an MLH Member Event,
+              if the event itself is creating an unsafe or inappropriate environment,
+              or if interacting with a MLH representative or event organizer makes you
+              uncomfortable, please report it using the procedures included in this
+              document.</i>
+            </p>
+
+              <p>
+                Major League Hacking (MLH) stands for inclusivity. We believe
+                that every single person has the right to hack in a safe
+                and welcoming environment.
+              </p>
+
+              <p>
+                Harassment includes but is not limited to offensive verbal or
+                written comments related to gender, age, sexual orientation, disability,
+                physical appearance, body size, race, religion, social class,
+                economic status, veteran status, sexual images, deliberate
+                intimidation, stalking, following, harassing photography or
+                recording, sustained disruption of talks or other events,
+                inappropriate physical contact, and unwelcome sexual attention.
+                If what you’re doing is making someone feel uncomfortable,
+                that counts as harassment and is enough reason to stop doing it.
+              </p>
+
+              <p>
+                Participants asked to stop any harassing behavior are expected
+                to comply immediately.
+              </p>
+
+              <p>
+                Sponsors, judges, mentors, volunteers, organizers, MLH staff,
+                and anyone else at the event are also subject to the anti-harassment
+                policy. In particular, attendees should not use sexualised images,
+                activities, or other material both in their hacks and during the event.
+                Booth staff (including volunteers) should not use sexualised
+                clothing/uniforms/costumes, or otherwise create a sexualised
+                environment.
+              </p>
+
+              <p>
+                If a participant engages in harassing behavior,
+                MLH may take any action it deems appropriate,
+                including warning the offender or expulsion
+                from the event with no eligibility for reimbursement
+                or refund of any type.
+              </p>
+
+              <p>
+                If you are being harassed, notice that someone else is being harassed,
+                or have any other concerns, please contact MLH using the
+                reporting procedures defined below.
+              </p>
+
+              <p>
+                MLH representatives will be happy to help participants contact
+                campus security or local law enforcement, provide escorts,
+                or otherwise assist those experiencing harassment to
+                feel safe for the duration of the event. We value your
+                attendance.
+              </p>
+
+              <p>
+                We expect participants to follow these rules at all hackathon
+                venues, online interactions in relation to the event,
+                hackathon-related social events, and on hackathon
+                supplied transportation.
+              </p>
+
+              <h2>Reporting Procedures</h2>
+
+              <p>
+                If you feel uncomfortable or think there may be a potential
+                violation of the code of conduct, please report it immediately
+                using one of the following methods. All reporters have the
+                right to remain anonymous.
+              </p>
+              <p>
+                By sending information to the general reporting line,
+                your report will go to any or all of the MLH
+                representatives listed below.
+              </p>
+
+              <ul>
+                <li> North America General Reporting - +1 409 202 6060, <a href=mailto:incidents@mlh.io>incidents@mlh.io</a> </li>
+                <li> Europe General Reporting - +44 800 808 5675, <a href=mailto:incidents@mlh.io>incidents@mlh.io</a </li>
+                <li> Asia-Pacific General Reporting - +91 80004 02492, <a href=mailto:incidents@mlh.io>incidents@mlh.io</a </li>
+              </ul>
+
+              <h2>Special Incidents</h2>
+
+              <p>
+                If you are uncomfortable reporting your situation to one or more
+                of these people or need to contact any of them directly in case of
+                emergency, direct contact details are listed below.
+              </p>
+
+              <ul>
+                <li> Chi Nguyen - +1 (586) 244-8877, <a href=mailto:chi@mlh.io>chi@mlh.io</a> </li>
+                <li> Jon Gottfried - +1 (212) 851-6746, <a href=mailto:jon@mlh.io>jon@mlh.io</a>  </li>
+                <li> Nick Quinlan - +1 (510) 859-8578, <a href=mailto:nq@mlh.io>nq@mlh.io</a>  </li>
+                <li> Ryan Swift - +1 (347) 868-6698, <a href=mailto:ryan@mlh.io>ryan@mlh.io</a>  </li>
+                <li> Swift - +1 (347) 220-8667, <a href=mailto:swift@mlh.io>swift@mlh.io</a>  </li>
+              </ul>
+
+              <p>
+                MLH reserves the right to revise, make exceptions to, or otherwise
+                amend these policies in whole or in part. If you have any
+                questions regarding these policies, please contact MLH by
+                h​i@mlh.io​.
+              </p>
+
+              <p style="text-align:center">
+                <i>Last updated - 26 November, 2019</a></i>
+              </p>
+
         </div>
     </section>
 @endsection
-
