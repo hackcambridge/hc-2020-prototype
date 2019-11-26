428f978c7de6b20f3c0953579d79c0c74b7faa2d
Harri Bell-Thomas <harribt@live.co.uk>
2019-11-26 15:44:02 +0000

    Now saves users' auth0 profiles

--- app/Repositories/CustomUserRepository.php
+++ app/Repositories/CustomUserRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\User;
+use Illuminate\Support\Facades\Log;
 
 use Auth0\Login\Auth0User;
 use Auth0\Login\Auth0JWTUser;
@@ -26,9 +27,6 @@ class CustomUserRepository extends Auth0UserRepository {
         if (!$user) {
             $user = new User();
             $user->setAttribute('sub', $profile['sub']);
-            $user->setAttribute('email', isset( $profile['email'] ) ? $profile['email'] : '');
-            $user->setAttribute('name', isset( $profile['name'] ) ? $profile['name'] : '');
-
             $sub_parts = explode("|", $profile['sub']);
             $type = ($sub_parts[0] == "email") ? "sponsor" :
                     (($sub_parts[1] == "MyMLH") ? "hacker" :
@@ -36,9 +34,18 @@ class CustomUserRepository extends Auth0UserRepository {
                         ? "committee" : "unknown")
                     );
             $user->setAttribute('type', $type);
-
-            $user->save();
+            $user->setAttribute('payload', '{}');
         }
+
+        $user->setAttribute('email', isset( $profile['email'] ) ? $profile['email'] : '');
+        $user->setAttribute('name', isset( $profile['name'] ) ? $profile['name'] : '');
+                
+        $payload = $this->getAuth0UserInformation($profile['sub']);
+        if($payload) {
+            $user->setAttribute('profile', $payload);
+        }
+
+        $user->save();
         return $user;
     }
 
@@ -66,4 +73,48 @@ class CustomUserRepository extends Auth0UserRepository {
         return new Auth0User($user->getAttributes(), $userinfo['accessToken']);
     }
 
+    public function getAuth0UserInformation($user_id){
+        // We generate a token:
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://hackcambridge.eu.auth0.com/oauth/token');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        
+        $client_id = env('AUTH0_CLIENT_ID');
+        $client_secret = env('AUTH0_CLIENT_SECRET');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"client_id\":\"".$client_id."\",\"client_secret\":\"".$client_secret."\",\"audience\":\"https://hackcambridge.eu.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}");
+        
+        $headers = array();
+        $headers[] = 'Content-Type: application/json';
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        }
+        curl_close($ch);
+
+        // TODO: Check if this is present!
+        $accessToken = json_decode($result)->access_token;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://hackcambridge.eu.auth0.com/api/v2/users/'.$user_id);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPGET, 1);
+        $headers = array();
+        $headers[] = 'Content-Type: application/json';
+        $headers[] = 'Authorization: Bearer '.$accessToken;
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            Log::error('Error:' . curl_error($ch));
+        }
+
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        if ($httpcode != "200") {
+            return false;
+        } else {
+            return $result;
+        }
+    }
 }
--- resources/views/frontpages/apply.blade.php
+++ resources/views/frontpages/apply.blade.php
@@ -8,9 +8,11 @@
 @endsection
 
 @section('content')
-    <section id="" class="section-diagonal static-section">
+    <section id="" class="section-diagonal static-section section-pink">
         <div class="container grid-lg" style="padding:2rem 30px 1rem;height:400px;">
             <h1 class="section-header">Apply for Hack Cambridge</h1>
+            <p>To start of continue your application, log in with MyMLH.</p>
+            <p>By authorising Hack Cambridge to view your MyMLH profile you agree to our <a href="{{ route('privacy') }}">Privacy Policy</a> and Terms and Conditions.
         </div>
     </section>
 @endsection
