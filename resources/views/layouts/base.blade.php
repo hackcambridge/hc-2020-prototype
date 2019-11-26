18f8b28aedb50af661f4bb575b2a0d6c7603efbc
Harri Bell-Thomas <harribt@live.co.uk>
2019-11-26 14:17:39 +0000

    Added Google Analytics code

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -19,4 +19,14 @@
 
         @yield('content')
     </body>
+            
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153422761-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-153422761-1');
+    </script>
 </html>
