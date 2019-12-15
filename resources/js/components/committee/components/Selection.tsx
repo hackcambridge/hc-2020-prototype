e1d9359367565f77976d109b7317f95d8152ed04
Tim Lazarus <me@timlazarus.com>
2019-12-15 21:30:33 +0000

    Flesh out functionality of editor

--- resources/js/components/committee/components/Selection.tsx
+++ resources/js/components/committee/components/Selection.tsx
@@ -1,35 +1,41 @@
 import React, { Component, ReactNode} from 'react';
-import { Page, Card, Tabs, Button, Modal, TextField } from "@shopify/polaris";
+import { Page, Card, Tabs, Button, Modal, TextField, Layout, Stack, Select } from "@shopify/polaris";
 import AceEditor from "react-ace";
 
 import "ace-builds/src-noconflict/mode-php";
 import "ace-builds/src-noconflict/theme-twilight";
+import "ace-builds/src-noconflict/ext-language_tools.js";
+
 
 interface ISelectionProps {
 
 }
 
 interface ISelectionState {
-    selected: number
+    selectedTab: number
     newFileModal: boolean
-    fileName: string
-    files: {id: string, content: string}[]
+    confirmDeleteModal: boolean
+    newFileName: string
+    files: {id: string, content: string, file_content: string}[]
 }
 
 class Selection extends Component<ISelectionProps, ISelectionState> {
 
     state = {
-        selected: 0,
+        selectedTab: 0,
         newFileModal: false,
-        fileName: "",
+        newFileName: "",
+        confirmDeleteModal: false,
         files: [
             {
                 id: 'overview',
                 content: 'Overview',
+                file_content: "",
             },
             {
                 id: 'test.php',
                 content: 'test.php',
+                file_content: "This is a test poo",
             }
         ]
     };
@@ -39,8 +45,8 @@ class Selection extends Component<ISelectionProps, ISelectionState> {
         return (
             <Page title="Selection">
                 <Card>
-                    <Tabs tabs={this.state.files} selected={this.state.selected} onSelect={this.handleTabChange} fitted>
-                        <Card.Section title={this.state.files[this.state.selected].content}>
+                    <Tabs tabs={this.state.files} selected={this.state.selectedTab} onSelect={this.handleTabChange} fitted>
+                        <Card.Section title={this.state.files[this.state.selectedTab].content}>
                             {this.renderPage()}
                         </Card.Section>
                     </Tabs>
@@ -48,7 +54,7 @@ class Selection extends Component<ISelectionProps, ISelectionState> {
 
                 <Modal
                     open={this.state.newFileModal}
-                    onClose={() => this.setState({ newFileModal: false, fileName:"" })}
+                    onClose={() => this.setState({ newFileModal: false, newFileName:"" })}
                     title="New File"
                     primaryAction={{
                         content: 'Create',
@@ -56,7 +62,27 @@ class Selection extends Component<ISelectionProps, ISelectionState> {
                     }}
                 >
                     <Modal.Section>
-                        <TextField label="File name" value={this.state.fileName} onChange={this.handleFileNameChange} />
+                        <TextField label="File name" value={this.state.newFileName} onChange={this.handleFileNameChange} />
+                    </Modal.Section>
+                </Modal>
+
+                <Modal
+                    open={this.state.confirmDeleteModal}
+                    onClose={() => this.setState({ confirmDeleteModal: false})}
+                    title="Are you sure?"
+                    primaryAction={{
+                        content: 'Delete',
+                        onAction: this.handleDelete
+                    }}
+                    secondaryActions={[
+                        {
+                            content: 'Cancel',
+                            onAction: () => this.setState({ confirmDeleteModal: false}),
+                        }
+                    ]}
+                >
+                    <Modal.Section>
+                        <p>This operation cannot be undone! Delete at your own risk!</p>
                     </Modal.Section>
                 </Modal>
             </Page>
@@ -64,7 +90,7 @@ class Selection extends Component<ISelectionProps, ISelectionState> {
     }
 
     private renderPage() : ReactNode {
-        if (this.state.selected == 0) {
+        if (this.state.selectedTab == 0) {
             return this.renderOverview();
         }
         return this.renderEditor();
@@ -72,16 +98,32 @@ class Selection extends Component<ISelectionProps, ISelectionState> {
 
     private renderEditor(): ReactNode {
         return (
-        <AceEditor
-            mode="php"
-            theme="twilight"
-            name="UNIQUE_ID_OF_DIV"
-            width="100%"
-            editorProps={{ $blockScrolling: true }}
-          />);
-
+            <Layout>
+                <Layout.Section>
+                <AceEditor
+                    mode="php"
+                    theme="twilight"
+                    name={this.state.files[this.state.selectedTab].id + 'EDITOR'}
+                    onChange={this.onChange}
+                    width="100%"
+                    enableBasicAutocompletion={true}
+                    enableLiveAutocompletion={true}
+                    enableSnippets={true}
+                    value={this.state.files[this.state.selectedTab].file_content}
+                />
+                </Layout.Section>
+                <Layout.Section>
+                    <Stack distribution="equalSpacing">
+                        <Button onClick={() => {
+                            this.setState({newFileModal: true});
+                        }} monochrome outline> Save</Button>
+                        <Button onClick={() => this.setState({confirmDeleteModal: true})} monochrome destructive> Delete</Button>
+                    </Stack>
+                </Layout.Section>
+            </Layout>
+          );
     }
-
+      
     private renderOverview() : ReactNode {
         return (
             <Button onClick={() => {
@@ -90,24 +132,38 @@ class Selection extends Component<ISelectionProps, ISelectionState> {
         );
     }
 
+    private onChange = (newValue:string) => {
+        this.state.files[this.state.selectedTab].file_content = newValue;
+    }
+
     private handleTabChange = (index:number) => {
-        this.setState({selected: index});
+        this.setState({selectedTab: index});
+    }
+
+    private handleDelete = () => {
+        
+        var newfiles = this.state.files;
+        newfiles.splice(this.state.selectedTab, 1)
+
+        this.setState({files: newfiles, selectedTab: this.state.selectedTab - 1, confirmDeleteModal: false})
     }
 
     private handleFileNameChange = (name:string) => {
-        this.setState({fileName: name});
+        this.setState({newFileName: name});
     }
 
     private addNewFile = () => {
-        this.setState({fileName: ""});
+        this.setState({newFileName: ""});
         this.setState({newFileModal: false});
 
-        // Loading stuff and call endpoint to save state
-
         this.state.files.push({
-            id: this.state.fileName,
-            content: this.state.fileName,
+            id: this.state.newFileName,
+            content: this.state.newFileName,
+            file_content: ""
         });
+        this.setState({selectedTab: (this.state.files.length - 1)})
+
+        // Loading stuff and call endpoint to save state
     }
 }
 
