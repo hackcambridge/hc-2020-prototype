5b981a2ac9fe0e27c4a0dc4abdb9a38e21f10b28
Tim Lazarus <me@timlazarus.com>
2019-12-15 03:03:18 +0000

    Add Selection page to Admin

---
+++ resources/js/components/committee/components/Selection.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Page } from "@shopify/polaris";
+
+class Selection extends Component {
+    render() {
+        return (
+            <Page title="Selection">
+
+            </Page>
+        );
+    }
+}
+
+export default Selection;
