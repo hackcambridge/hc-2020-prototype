13d382458012fb8d5683e004136276824413040c
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-25 21:24:22 +0000

    Merged harri/circleci-tweaks

--- config/database.php
+++ config/database.php
@@ -49,6 +49,25 @@ return [
             'prefix'   => 'test_',
         ],
 
+        'testing_database' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'hc',
+            'username' => 'hc',
+            'password' => 'secret',
+            'unix_socket' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => 'test_',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
