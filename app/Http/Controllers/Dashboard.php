b6af361929d2db3f02f5351d3a393c3b80744e23
Harri Bell-Thomas <harribt@live.co.uk>
2019-12-31 16:55:11 +0000

    Closing applications

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -14,7 +14,7 @@ use App\Http\Resources\TeamMember as TeamMemberResource;
 class Dashboard extends Controller
 {
     private $maximum_team_size = 4;
-    private $accepting_applications = true;
+    private $accepting_applications = false;
 
     public function index() {
         return view('dashboard/index');
