fe260d6bfec2cb132853ec0c297aebdfd380864e
Harri Bell-Thomas <harribt@live.co.uk>
2020-02-25 13:14:18 +0000

    Removed canary

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,6 @@
 #   value = module.hc-prod-cluster.domain-name
 # }
 
-output "staging-domain-name" {
-  value = module.hc-staging-cluster.domain-name
-}
+# output "staging-domain-name" {
+#   value = module.hc-staging-cluster.domain-name
+# }
