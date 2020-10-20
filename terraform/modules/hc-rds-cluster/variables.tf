aacfe79f50285a9ddd82cc3d9ae98d33c57d61a8
Ivan Ivanov <ivan@vankata.tech>
2020-10-21 00:02:42 +0100

    Fix environment variables

--- terraform/modules/hc-rds-cluster/variables.tf
+++ terraform/modules/hc-rds-cluster/variables.tf
@@ -2,3 +2,13 @@ variable "name" {
   description = "The name of the database cluster"
   default     = ""
 } 
+
+# --------------------------------------------
+# Environment variables to set on the machines
+# --------------------------------------------
+variable "DB_USERNAME" {
+  default = "homestead"
+}
+variable "DB_PASSWORD" {
+  default = "secret"
+}
