037e749fceb9c19703464287dd5810c4d50cc4b0
Ivan Ivanov <ivan@vankata.tech>
2020-11-14 00:38:07 +0000

    Disable apply page

--- resources/views/frontpages/apply.blade.php
+++ resources/views/frontpages/apply.blade.php
@@ -5,14 +5,14 @@
     <section id="" class="section-apply">
         <div class="container grid-lg" style="padding:2rem 30px 1rem;">
             <h1 class="section-header">Apply for Hex Cambridge</h1>
-            @if($open)        
+            <!-- @if($open)        
                 <a href="{{ route('dashboard_index') }}">
                     <img id="register-button" src="{{ asset('images/logo.svg') }}" />
                 </a>
-            @else
+            @else -->
                 <br />
                 <h4 class="section-header">Applications have not opened yet!</h4>
-            @endif
+            <!-- @endif -->
             <!-- <p>To start of continue your application, log in with MyMLH.</p> -->
             <p class="body-text">By authorising Hex Cambridge to view your MyMLH profile you agree to our <a href="{{ route('privacy') }}">Privacy Policy</a> and <a href="{{ route('terms') }}">Terms and Conditions</a>.
         </div>
