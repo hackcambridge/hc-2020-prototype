e2381b96ac23afa2a9603953acc394804bee975a
MilkFansHelloWorld <31027215+MilkFansHelloWorld@users.noreply.github.com>
2021-08-28 15:23:03 +0100

    [DATABASE CLEANUP] Destroy staging instances

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,9 +2,9 @@ output "prod-domain-name" {
   value = module.hc-prod-cluster.domain-name
 }
 
-output "staging-domain-name" {
-  value = module.hc-staging-cluster.domain-name
-}
+//output "staging-domain-name" {
+//  value = module.hc-staging-cluster.domain-name
+//}
 
 output "rds-domain-name" {
   value = module.hc-2022-rds-cluster.endpoint
