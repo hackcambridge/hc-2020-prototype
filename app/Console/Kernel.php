a14212f22965f391ed516048aaceea67915fa57f
Theo Wang <theo.wang@arm.com>
2021-12-22 16:04:10 +0000

    Change to 5 min to test and corrected bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->call(new UpdateMailchimp())->hourly()->withoutOverlapping();
+        $schedule->call(new UpdateMailchimp())->everyFiveMinutes();
     }
 
     /**
