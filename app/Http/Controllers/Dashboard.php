5f82d481c24a1cde33f41b77c652b5000383311a
Theo Wang <theo.wang@arm.com>
2022-01-06 17:33:09 +0000

    Changed a few things on front page: logo and faqs

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -19,7 +19,7 @@ use Illuminate\Validation\UnauthorizedException;
 class Dashboard extends Controller
 {
     private $maximum_team_size = 5;
-    private static $accepting_applications = true;
+    private static $accepting_applications = false;
     private static $discord_invite_url = "https://discord.gg/kBahBx4Vwa";
 
     public function index()
